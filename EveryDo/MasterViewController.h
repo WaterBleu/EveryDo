3759cc6b3db614649877b1a2d055fd7903acc4c1
WaterBleu <jeff7746@hotmail.com>
2015-07-15 09:56:42 -0700

    Initial Commit

---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Jeff Huang on 2015-07-15.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
